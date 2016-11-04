397e3d4e7729e612e5c4f18bd855e2517bd6c93b
Daniel Gray <dg@danielgray.me>
2016-11-04 20:56:35 +0000

    Dank updatez
    Gotta update 'em all!

--- MyBot.cs
+++ MyBot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,8 +36,6 @@ namespace Discord_Bot
                 });
 
             var commands = discord.GetService<CommandService>();
-            //make sure to put your token here
-            var ownertoken = 137957400765267968;
 
             commands.CreateCommand("Status")
                 .Description("Checks the bots status")
@@ -45,6 +43,49 @@ namespace Discord_Bot
                     {
                         await e.Channel.SendMessage("```Discord-Bot Status: \n Version: " + Environment.Version + "\n Running on: " + Environment.OSVersion + "\n Running from file location: " + Environment.CurrentDirectory + "``` \n And made by https://danielgray.me!");
                     });
+            commands.CreateCommand("terminate")
+                .AddCheck((cmd, u, ch) => u.Id == 137957400765267968)
+                .Do(async (e) =>
+                    {
+                        await e.Channel.SendMessage("Goodbye world!(Shut down by owner)");
+                        Environment.Exit(0);
+                    });
+            commands.CreateCommand("memes")
+                .Description("Providing the best memes for your daily entertainment")
+                .Do(async (e) =>
+                    {
+                        Random meme = new Random();
+                        var memetoshow = meme.Next(1, 11);
+                        if (memetoshow == 1)
+                        {
+                            await e.Channel.SendMessage("https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FVdb_ParFCks%2Fmaxresdefault.jpg&f=1");
+                        }
+                        else if (memetoshow == 2)
+                        {
+                            await e.Channel.SendMessage("https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.ifcdn.com%2Fimages%2F30f9c0651e51719c64f59c309684b115d9752bd7dc76e11f6c3589433f047050_1.jpg&f=1");
+                        }
+                        else if (memetoshow == 3)
+                        {
+                            await e.Channel.SendMessage("https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgflip.com%2F155ngq.jpg&f=1");
+                        }
+                        else if (memetoshow == 4)
+                        {
+                            await e.Channel.SendMessage("https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FVrC42YOcLU4%2Fmaxresdefault.jpg&f=1");
+                        }
+                        else if (memetoshow == 5)
+                        {
+                            await e.Channel.SendMessage("https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F000%2F875%2F516%2Fde9.jpg&f=1");
+                        }
+
+                    });
+
+            discord.UserJoined += async (s, e) =>
+                 {
+                     Console.WriteLine("[" + e.Server.Name + "] " + e.User.Name + "#" + e.User.Discriminator + " Has joined the server");
+                     await e.Server.DefaultChannel.SendMessage("Please welcome " + e.User.Mention + " to the server! Make sure you obey the rules and enjoy!");
+                 };
+
+           
 
 
             //startup and login (For closed source program there is a input for the token, feel free to remove if its only you using)
